6046ae8376bad9416d243d05abe7a5790491cef9
Sirmatty17 <Iyanuoluwagbemiga575@gmail.com>
2023-01-10 13:32:55 +0000

    Addition Module

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition Function
+
+    Arguments:
+    a = First integer
+    b = Second integer
+
+    Return: Return value a + b
+    """
+    return (a + b)
