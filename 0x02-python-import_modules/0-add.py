3628433d712c483afef4ab59c58053176bb4dbad
Sirmatty17 <Iyanuoluwagbemiga575@gmail.com>
2023-01-10 14:14:42 +0000

    module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """print some of 1 and 2"""
     from add_0 import add
@@ -6,4 +7,3 @@ if __name__ == "__main__":
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,36 +1,43 @@
 #!/usr/bin/python3
 def add(a, b):
     """My addition function
-    Arg:
-    a: first integer
-    b: second integer
-    return: value of a + b
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
     """
     return (a + b)
 
+
 def sub(a, b):
     """My subtraction function
-    Arg:
-    a: first integer
-    b: second integer
-    Return: value of a - b
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a - b
     """
     return (a - b)
 
+
 def mul(a, b):
-    """My Multiplication Function
-     Arg:
-     a: First integer
-     b: Second integer
-     Return: value of a * b
-     """
+    """My multiplication function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a * b
+    """
     return (a * b)
 
+
 def div(a, b):
-    """My Division Function
-    Arg:
-    a: First integer
-    b: Second integer
-    Return: value of a / b
+    """My division function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a / b
     """
     return int(a / b)
