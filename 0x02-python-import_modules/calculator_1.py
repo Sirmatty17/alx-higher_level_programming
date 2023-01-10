cb8a289f383ac15bafacf437270619cc8e73b23b
Sirmatty17 <Iyanuoluwagbemiga575@gmail.com>
2023-01-10 14:03:41 +0000

    Simple Calculation module

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Arg:
+    a: first integer
+    b: second integer
+    return: value of a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+    Arg:
+    a: first integer
+    b: second integer
+    Return: value of a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My Multiplication Function
+     Arg:
+     a: First integer
+     b: Second integer
+     Return: value of a * b
+     """
+    return (a * b)
+
+def div(a, b):
+    """My Division Function
+    Arg:
+    a: First integer
+    b: Second integer
+    Return: value of a / b
+    """
+    return int(a / b)
