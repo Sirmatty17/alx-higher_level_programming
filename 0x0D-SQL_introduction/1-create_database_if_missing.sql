622a79f790d95b23d626d44fde90a849439b7369
Sirmatty17 <Iyanuoluwagbemiga575@gmail.com>
2023-03-22 14:14:56 +0000

    Sql-Introduction

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- create a database and works even if already exists
-CREATE DATABASE IF NOT EXISTS "hbtn_0c_0";
+CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Delete database and throw no error if not exists
-DROP DATABASE IF EXISTS "hbtn_0c_0";
+DROP DATABASE IF EXISTS 'hbtn_0c_0';
