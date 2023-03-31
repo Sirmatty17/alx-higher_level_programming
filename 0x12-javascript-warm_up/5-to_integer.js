83a8cb2a5408211a7ece34ebe9f66db0f3640440
Sirmatty17 <Iyanuoluwagbemiga575@gmail.com>
2023-03-31 09:03:10 +0000

    Print My Number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const Math.floor(Number(process.argv[2]));
-console.log(isNaN(Num) `Not a Number` : `My Number : ${Num}`);
+console.log(isNaN(Num) 'Not a Number' : `My Number : ${Num}`);
